99c27c55df06ac952876d05d4d5b27abed7db33b
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2014-12-28 23:33:17 -0500

    Copiado de librerias frontend desde bower

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,12 @@ var reactify = require('reactify');
 var source = require('vinyl-source-stream');
 var fs = require('fs');
 var path = require('path');
+var preen = require('preen');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var gutil = require('gulp-util');
+
+var config = { production: false };
 
 // Scripts
 var appDir = "./src/app/";
@@ -17,7 +23,18 @@ var componentBuildDir = "./build/js/";
 
 var vendorDir = "./build/js/vendor/"
 var vendorModules = [
-    ["react/addons", "react-with-addons.js"]
+    {
+        bowerName: "react",
+        devSource: "react-with-addons.js",
+        prodSource: "react-with-addons-min.js",
+        destName: "react-with-addons"
+    },
+    {
+        bowerName: "showdown",
+        devSource: "src/showdown.js",
+        prodSource: "compressed/showdown.js",
+        destName: "showdown.js"
+    }
 ];
 
 // Css
@@ -25,43 +42,52 @@ var cssDir = './src/css/';
 var cssPaths = fs.readdirSync(cssDir).map(function(item){return './' + path.join(cssDir, item)});
 var cssBuildDir = './build/css/';
 
+gulp.task('set-production', function(done){
+    config.production = true;
+    done();
+});
+
 gulp.task('clean', function(done){
     del(['build'], done);
 });
 
-gulp.task('browserify', ['clean'], function(){
-    // Bundle vendor modules
+gulp.task('preen', ['clean'], function(done){
+    preen.preen({}, done);
+});
 
-    vendorModules.forEach(function(item){
-        var b = browserify();
-        b.require(item[0])
-            .bundle()
-            .pipe(source(item[1]))
-            .pipe(gulp.dest(vendorDir));
+gulp.task('copy-vendor', ['preen'], function(){
+    var vendorPathList = vendorModules.map(function(item){
+        var moduleName = config.production?item.prodSource:item.devSource;
+        var modulePath = path.join("bower_components", item.bowerName ,moduleName);
+        return modulePath
     });
-    // Bundle react-components
-    componentPaths.forEach(function(src){
-        var b = browserify();
-        b.transform(reactify);
-        b.external(vendorModules.map(function(item){return item[0]}));
-        b.require("./" + path.join(componentDir, src));
-        b.bundle();
-    });
+    return gulp.src(vendorPathList, {base: "bower_components"})
+        .pipe(rename(function(filePath){
+            filePath.dirname = "";
+        }))
+        .pipe(gulp.dest(vendorDir));
+});
+
+gulp.task('browserify',  ['copy-vendor'], function(){
     //Bundle apps
     appPaths.forEach(function(src){
-        var b = browserify();
+        b = browserify();
         b.transform(reactify);
-        b.external(vendorModules.map(function(item){return item[0]}));
         b.add("./" + path.join(appDir, src));
         b.bundle()
             .pipe(source(src.split(".")[0] + '.js'))
-            .pipe(gulp.dest(appBuildDir));
+            .pipe(gulp.dest(appBuildDir))
     });
+    
 });
 
-gulp.task ('css', ['clean'], function(){
+gulp.task ('css', ['browserify'], function(){
     return gulp.src(cssPaths)
-    .pipe(gulp.dest(cssBuildDir));
+        .pipe(gulp.dest(cssBuildDir));
 });
 
-gulp.task('default', ['browserify', 'css']);
+// Produccion
+gulp.task('deploy', ['set-production', 'default']);
+
+// Por defecto, desarrollo
+gulp.task('default', ['copy-vendor', 'browserify', 'css']);
