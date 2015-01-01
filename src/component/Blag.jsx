82e3fd9ddc8b9b3079f7cef385f53233d118446b
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2015-01-01 03:36:11 -0500

    PropTypes de blag

--- src/component/Blag.jsx
+++ src/component/Blag.jsx
@@ -4,6 +4,21 @@ var BlogHeader = require('./BlogHeader.jsx');
 var PostList = require('./PostList.jsx');
 
 var Blag = React.createClass({
+    propTypes:{
+        data: React.PropTypes.shape({
+            blog: React.PropTypes.shape({
+                titulo: React.PropTypes.string,
+                subtitulo: React.PropTypes.string
+            }),
+            page: React.PropTypes.oneOf(['main', 'post']),
+            posts: React.PropTypes.arrayOf(
+                React.PropTypes.shape({
+                    // La forma de meta aun no esta bien definida
+                    meta: React.PropTypes.object,
+                    content: React.PropTypes.string
+                }))
+        })
+    },
     render: function(){
         var data = this.props.data;
         return(
