fd6486c05a2330c313669bb7bd317567081fe4cf
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2015-01-01 03:33:37 -0500

    Link de post no cambian la pagina

--- src/component/PostHeader.jsx
+++ src/component/PostHeader.jsx
@@ -3,15 +3,21 @@ var AutorLabel  = require('./AutorLabel.jsx');
 var PostDate = require('./PostDate.jsx');
 
 var PostHeader = React.createClass({
+    linkOnClickHandler: function(event){
+        event.preventDefault();
+        console.log("Link", this.refs.postLink.props.href);
+    },
     render: function(){
         var postData = this.props.postData;
         var meta = postData.meta;
         var titulo = meta.titulo;
         var autor = meta.autor;
         var fecha = meta.fecha;
+        var postUrl = "/post/" + meta.id;
         return(
-            <header>
-                <h2>{titulo}</h2>
+            <header className="blogPostHeader">
+                <a ref="postLink"
+                   href={postUrl} onClick={this.linkOnClickHandler}><h2>{titulo}</h2></a>
                 <p> {"por "}
                     <AutorLabel autor={autor}/>
                     {" el "}
