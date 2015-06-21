77e08f238f55d20f53454d22aaa402130dda9f2a
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2015-06-21 02:51:57 -0500

    PostList ahora no puede recibir solo un post

--- src/component/PostList.jsx
+++ src/component/PostList.jsx
@@ -3,16 +3,9 @@ var Post = require('./Post.jsx');
 
 var PostList = React.createClass({
     render: function(){
-        var data = this.props.data;
-        var listaPosts = data.posts;
-        var resumido = true;
-        if (data.page == 'post')
-        { // Nos aseguramos que solo hay un elemento en la lista
-          listaPosts = [listaPosts[0]];
-          resumido = false;
-        };
+        var listaPosts = this.props.posts;
         var postDescriptors = listaPosts.map(function(postData){
-            return <Post data={postData} resumido={resumido} key={"post-" + postData.meta.id}/>;
+            return <Post {...postData} resumido={true} key={"post-" + postData.meta.id}/>;
         });
         return(
             <section id="blogPostList">
