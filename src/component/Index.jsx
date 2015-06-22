a043f748fc533d179ba0f43c331644ede94f284e
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2015-06-22 08:22:07 -0500

    Index.jsx actualizado

--- src/component/Index.jsx
+++ src/component/Index.jsx
@@ -5,22 +5,20 @@ var PostList = require('./PostList.jsx');
 
 var Index = React.createClass({
     propTypes:{
-        data: React.PropTypes.shape({
-            blog: React.PropTypes.shape({
-                titulo: React.PropTypes.string,
-                subtitulo: React.PropTypes.string
-            }),
-            page: React.PropTypes.oneOf(['main', 'post']),
-            posts: React.PropTypes.arrayOf(
-                React.PropTypes.shape({
-                    // La forma de meta aun no esta bien definida
-                    meta: React.PropTypes.object,
-                    content: React.PropTypes.string
-                }))
-        })
+        blog: React.PropTypes.shape({
+            titulo: React.PropTypes.string,
+            subtitulo: React.PropTypes.string
+        }),
+        page: React.PropTypes.oneOf(['main', 'post']),
+        posts: React.PropTypes.arrayOf(
+            React.PropTypes.shape({
+                // La forma de meta aun no esta bien definida
+                meta: React.PropTypes.object,
+                content: React.PropTypes.string
+            }))
     },
     render: function(){
-        var data = this.props.data;
+        var data = this.props;
         return(
             <div>
                 <BlogHeader {...data.blog}/>
