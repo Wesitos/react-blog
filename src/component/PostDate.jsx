e513a99db49d8d68ba5adef940e1962526b7990b
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2015-06-26 09:34:10 -0500

    Arreglado fecha de post

--- src/component/PostDate.jsx
+++ src/component/PostDate.jsx
@@ -4,7 +4,7 @@ var PostDate = React.createClass({
     render: function(){
         var date = new Date(this.props.fecha.split('-'));
         var listaFecha = [date.getUTCDate(),
-                          date.getUTCMonth(),
+                          date.getUTCMonth()+1,
                           date.getUTCFullYear()];
         var strFecha =  listaFecha.reduce(function(x,y){
             return x + "/" + y;
