0d0a390350d1985007d89a2d21a5c9a3c1e8ad41
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2015-06-26 02:10:12 -0500

    Agregado yaml-front-matter

--- buildPosts.js
+++ buildPosts.js
@@ -1,14 +1,17 @@
+'use strict';
+
 var through = require('through2');
 var  gutil = require('gulp-util');
 var Path = require('path');
+var yamlFront = require('yaml-front-matter');
 
 // relative path and base path
-function makeJson(filetext, relative) {
+function makeJson(dataDict, relative) {
     var nameParams = relative.split(Path.sep).pop().split(":"); // [date, [id,] title]
     var fecha = nameParams[0];
     var postId = [nameParams[0],(nameParams.length<3)?1:nameParams[1]].join(":");
     var autor = relative.split(Path.sep)[0];
-    var titulo = nameParams[nameParams.length-1].replace(".md", "").replace(/-/g, " ");
+    var titulo = (dataDict.titulo || nameParams[nameParams.length-1].replace(".md", "").replace(/-/g, " ")).toLowerCase();
     var path = [autor, fecha.replace(/-/g,"/"), titulo.replace(/ /g,"-").replace(/$/, ".json")].join("/");
     var obj = {
         post: {
@@ -19,7 +22,7 @@ function makeJson(filetext, relative) {
                 titulo: titulo,
                 url: ["", path.replace(/\.json$/, ".html")].join("/")
             },
-            content: filetext
+            content: dataDict.content
         }};
     return {content:JSON.stringify(obj), path: path};
 };
@@ -27,7 +30,7 @@ function makeJson(filetext, relative) {
 function buildPosts(){
     // Creating a stream through which each file will pass
     return through.obj(function(file, enc, cb) {
-        var data = makeJson(file.contents.toString(), file.relative);
+        var data = makeJson(yamlFront.loadFront(file.contents, 'content'), file.relative);
         gutil.log("buildPosts", file.relative);
         if (file.isNull()) {
             // return empty file
