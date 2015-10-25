961b7cc04671288a395812933de150a6e87e6df8
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2015-10-25 03:57:46 -0500

    Renderizado de paginas utilizando async

--- scripts/render.js
+++ scripts/render.js
@@ -3,6 +3,7 @@ var glob = require("glob");
 var fs = require("fs");
 var mkdirp = require("mkdirp");
 var gutil = require("gulp-util");
+var async = require("async");
 
 var cloneElement = require('react').cloneElement;
 var ReactDOMServer = require('react-dom/server');
@@ -28,24 +29,47 @@ var renderPage = function(data, html, appName){
     return output;
 };
 
-var renderAll = function(progress, cb){
-    routes.routes.forEach(function (route){
-        var filenames = glob.sync(Path.join("./build/json", route.data));
-        filenames.forEach(function(filename){
-            var data = require(["..",filename].join("/"));
-            var html = fs.readFileSync(Path.join('./src/html', route.html),'utf-8');
-            var output = renderPage(data, html, route.app);
-            // Cambiamos la extension del nombre
-            var baseName = [Path.basename(filename, Path.extname(filename)), ".html"].join("");
-            var dirName = Path.join(Path.dirname(filename).replace("build/json", Path.join("build", route.route)));
-            var path = Path.join(dirName, baseName);
+var renderAll = function(progress, done){
+    // Procesa cada ruta
+    async.each(routes.routes, processRoute,
+               function(err){
+                   // Aca terminamos la ejecucion de toda la funcion
+                   done();
+               });
 
-            //Verificamos si las carpetas necesarias existen
-            mkdirp.sync(Path.dirname(path));
-            fs.writeFileSync(path, output);
-            progress(filename, path);
-        });
-    });
-        cb();
+    function processRoute(route, routeCallback){
+        var filenames = glob(Path.join("./build/json", route.data),
+                             renderFileBatch);
+
+        function renderFileBatch(err, filenames){
+            async.each(filenames, renderFile, function(err){
+                // Aca terminamos con esta ruta
+                routeCallback();
+            });
+
+            function renderFile(filename, fileCallback){
+                var data = require(Path.join("..",filename));
+                var html = fs.readFileSync(Path.join('./src/html', route.html),'utf-8');
+                var output = renderPage(data, html, route.app);
+                // Cambiamos la extension del nombre
+                var baseName = [Path.basename(filename, Path.extname(filename)), ".html"].join("");
+                var dirName = Path.join(Path.dirname(filename).replace("build/json", Path.join("build", route.route)));
+                var path = Path.join(dirName, baseName);
+
+                // Verificamos si las carpetas necesarias existen antes de guardar
+                // el html
+                mkdirp(Path.dirname(path), saveOutput);
+
+                // Funcion para guardar el html
+                function saveOutput(){
+                    fs.writeFile(path, output, function(err){
+                        // Aca acabamos con el archivo
+                        progress(filename, path);
+                        fileCallback();
+                    });
+                };
+            }
+        }
+    };
 };
 module.exports = renderAll;
